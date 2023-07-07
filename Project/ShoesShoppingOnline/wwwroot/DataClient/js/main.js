69bbdf71db1a67e0c7ebc15eac4061fd2ccb9455
chienITrbn <chienjobit@gmail.com>
2023-07-07 10:50:09 +0700

    modify AddToCart

--- Project/ShoesShoppingOnline/wwwroot/DataClient/js/main.js
+++ Project/ShoesShoppingOnline/wwwroot/DataClient/js/main.js
@@ -194,22 +194,22 @@ upsell-slider
 /*----------------------------
  cart-plus-minus-button active
 ------------------------------ */
-    $(".cart-plus-minus").append('<div class="dec qtybutton"><</div><div class="inc qtybutton">></div>');
-    $(".qtybutton").on("click", function() {
-        var $button = $(this);
-        var oldValue = $button.parent().find("input").val();
-        if ($button.text() == ">") {
-            var newVal = parseFloat(oldValue) + 1;
-        } else {
-            // Don't allow decrementing below zero
-            if (oldValue > 0) {
-                var newVal = parseFloat(oldValue) - 1;
-            } else {
-                newVal = 0;
-            }
-        }
-        $button.parent().find("input").val(newVal);
-    });
+    //$(".cart-plus-minus").append('<div class="dec qtybutton"><</div><div class="inc qtybutton">></div>');
+    //$(".qtybutton").on("click", function() {
+    //    var $button = $(this);
+    //    var oldValue = $button.parent().find("input").val();
+    //    if ($button.text() == ">") {
+    //        var newVal = parseFloat(oldValue) + 1;
+    //    } else {
+    //        // Don't allow decrementing below zero
+    //        if (oldValue > 0) {
+    //            var newVal = parseFloat(oldValue) - 1;
+    //        } else {
+    //            newVal = 0;
+    //        }
+    //    }
+    //    $button.parent().find("input").val(newVal);
+    //});
 
 /*--------------------------
  scrollUp
